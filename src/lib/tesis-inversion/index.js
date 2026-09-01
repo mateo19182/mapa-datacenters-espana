fd3361d1d0c9f59614fec3dacf1c084dbe32260a
m19182-server <mateoamadoares@gmail.com>
2026-09-01 11:19:43 +0200

    Añadir cinco tesis de inversión sectoriales

---
+++ src/lib/tesis-inversion/index.js
@@ -0,0 +1,8 @@
+import redeia from './redeia.js'
+import acs from './acs.js'
+import iberdrola from './iberdrola.js'
+import solaria from './solaria.js'
+import ferrovial from './ferrovial.js'
+
+export const tesisEmpresas = { redeia, acs, iberdrola, solaria, ferrovial }
+export const tesisEmpresasLista = Object.values(tesisEmpresas)
